9a0a7f80e07730643925c0f0b6515950a31f84d9
Guilherme veber <guilhermeveber2@gmail.com>
2026-06-27 21:26:03 -0300

    Modulo Condicionais

---
+++ secao5/Condicionais.java
@@ -0,0 +1,52 @@
+package secao5;
+
+public class Condicionais {
+ 
+    public static void main(String[] args) {
+     
+        // 1 - o que é boolean?
+
+        boolean isTrue = true;
+        boolean isFalse = false;
+
+        System.out.println(isTrue);
+        System.out.println(isFalse);
+
+        // 2 - Operadores de comparação
+        
+        int x = 10;
+
+        System.out.println(x == 10); // true
+
+        System.out.println(x == 9); // false
+
+        System.out.println(x != 5); // true
+
+        System.out.println(x != 10); // false
+
+        System.out.println(x > 10); // false
+
+        // x é maior ou igual a 10
+        System.out.println(x >= 10); // true
+
+        // x é menor ou igual a 10
+        System.out.println(x <= 10); // true
+
+        // x é menor ou igual a 10
+        System.out.println(x < 10); // false
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+}
